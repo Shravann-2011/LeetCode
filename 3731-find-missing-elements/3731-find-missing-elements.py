7a2101005fbf7465d1214e835684568486c0263c
Shravann-2011 <shravannpk@gmail.com>
2026-08-04 14:58:08 +0530

    Time: 10 ms (26.5%), Space: 12.2 MB (87.79%) - LeetHub

---
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def findMissingElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums_set = set(nums)
+        list = []
+        left = min(nums_set)
+        right = max(nums_set)
+        for i in range(left,right):
+            if i not in nums:
+                list.append(i)
+        return list
+
+        
+
+
+        
