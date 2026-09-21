f52d14fdb6e877de01def09f625b34cdff6601f5
Shravann-2011 <shravannpk@gmail.com>
2026-09-21 22:36:28 +0530

    Time: 252 ms (66.98%), Space: 66.8 MB (47.16%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -10,37 +10,12 @@ class Solution(object):
         :type head1, head1: ListNode
         :rtype: ListNode
         """
-        
-        lenA = 0
-        lenB = 0
-        a = headA
-        b = headB
-
-        while a is not None:
-            lenA+=1
-            a = a.next
-        
-        while b is not None:
-            lenB+=1
-            b = b.next
 
         a = headA
         b = headB
 
-        while lenA > lenB:
-            a = a.next
-            lenA-=1
+        while a != b:
+            a = headB if a is None else a.next
+            b = headA if b is None else b.next
 
-        while lenB > lenA:
-            b = b.next
-            lenB-=1
-        
-        while a!=b:
-            a = a.next
-            b = b.next
         return a
-        
-
-
-
-
